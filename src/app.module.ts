d33f966c7cd796013eb61234987c1a6779918205
Yael Ruiz <yaelruiz99@outlook.com>
2024-06-30 22:35:51 -0600

    Envorment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppConfig } from './config/app.config';
+import { EnvValidationSchema } from './config/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [AppConfig],
+      validationSchema: EnvValidationSchema,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
