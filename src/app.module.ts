f836fb32ecd0d99967756d928deb30849db5b021
Yael Ruiz <yaelruiz99@outlook.com>
2024-06-30 22:40:51 -0600

    Config graphql and todo resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { join } from 'path';
 import { ConfigModule } from '@nestjs/config';
 import { AppConfig } from './config/app.config';
 import { EnvValidationSchema } from './config/env.validation';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
@@ -9,6 +14,13 @@ import { EnvValidationSchema } from './config/env.validation';
       load: [AppConfig],
       validationSchema: EnvValidationSchema,
     }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TodoModule,
   ],
   controllers: [],
   providers: [],
